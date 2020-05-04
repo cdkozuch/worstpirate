04d2b1dec0e3d76ba26233fbf633e8e092c1b9e8
Christopher Kozuch <chris.kozuch@gmail.com>
2020-05-04 17:25:51 -0500

    enabled waitress server

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return "That is, without doubt, the worst pirate I've ever seen."
+@app.route("/")
+def index():
+    return "<h1>That is, without doubt, the worst pirate I've ever seen.</h1>"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=80)
