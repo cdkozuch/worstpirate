2acf7548089087ac7ce6d2fbb8e2254149539580
Christopher Kozuch <chris.kozuch@gmail.com>
2020-05-11 18:26:33 -0500

    added templates and evironment option

--- src/fractal.py
+++ src/fractal.py
@@ -47,10 +47,7 @@ image = np.zeros((500 * 2, 750 * 2), dtype=np.uint8)
 create_fractal(-2.0, 1.0, -1.0, 1.0, image, 20)
 imshow(image)
 
-def do_plot():
-    plt.plot([1,2,3], [1,2,3])
-
-    # here is the trick save your figure into a bytes object and you can afterwards expose it via flas
+def generate_plot():
     bytes_image = io.BytesIO()
     plt.savefig(bytes_image, format='png')
     bytes_image.seek(0)
--- src/main.py
+++ src/main.py
@@ -1,22 +1,32 @@
-from flask import Flask, send_file, make_response
-from doplot import do_plot
+from flask import Flask, send_file, make_response, render_template
+from argparse import ArgumentParser
+
+from fractal import generate_plot
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return "<h1>That is, without doubt, the worst pirate I've ever seen.</h1>"
+    return render_template('index.html')
 
 @app.route('/plot', methods=['GET'])
-def correlation_matrix():
-    bytes_obj = do_plot()
+def get_plot():
+    bytes_obj = generate_plot()
     
     return send_file(bytes_obj,
                      attachment_filename='plot.png',
                      mimetype='image/png')
 
 if __name__ == "__main__":
-    #app.debug = True
-    #app.run(host="0.0.0.0", port=8080)
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=80)
+    parser = ArgumentParser("Simple Web App")
+    parser.add_argument('-e', '--flask_env', required=True, type=str,
+        choices=['prod', 'dev'], help="choose production or development environment")
+
+    args = parser.parse_args()
+
+    if args.flask_env == 'prod':
+        from waitress import serve
+        serve(app, host="0.0.0.0", port=80)
+    else:
+        app.debug = True
+        app.run(host="0.0.0.0", port=8080)
